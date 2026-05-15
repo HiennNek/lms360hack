7a51c8035f34ffa2bf4ae57d5380da2e80db2f05
HiennNek <trdahien2011@gmail.com>
2026-05-15 08:55:22 +0700

    Optimize css and font loading

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource-variable/material-symbols-outlined';
-import './styles/main.css';
+import './styles/index.css';
 import { initThemeToggle } from './modules/theme-toggle';
 import { initProcessQuestionType } from './modules/process-question-type';
 
